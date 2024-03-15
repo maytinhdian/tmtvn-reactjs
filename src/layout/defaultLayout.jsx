bd2f34aed8dad08103e60e81db9050c7b1c4f913
Minh Triet <minhtriet@maytinhdian.com>
2024-03-15 16:28:36 +0700

    Install MUI

---
+++ src/layout/defaultLayout.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+class defaultLayout extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default defaultLayout;
